2d266e3cc8246c840462593c0353ef5cc41b4c41
Michael21290 <74963854+Michael21290@users.noreply.github.com>
2021-02-03 14:41:04 +0100

    WPF Programm mit Klassen hinzugefügt
    Erste Idee einer WPF Anwendung  mit Klassen

---
+++ Projekt2/Projekt2/Personal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2
+{
+    abstract class Personal
+    {
+        public string ID { get; }
+        public string Name { get; set; }
+        public string Vorname { get; set; }
+
+        public DateTime Geburtsdatum = new DateTime();
+
+        public DateTime Einstellungsdatum = new DateTime();
+
+        public string Stellenbezeichnung { get; set; }
+        public string Email { get; set; }
+        public bool Verfuegbar { get; set; }
+
+        public Personal(string Name, string Vorname, DateTime Geburtsdatum, DateTime Einstellungsdatum, string Stellenbezeichnung, string Email, bool Verfuegbar)
+        {
+            this.Name = Name;
+            this.Vorname = Vorname;
+            this.Geburtsdatum = Geburtsdatum;
+            this.Einstellungsdatum = Einstellungsdatum;
+            this.Stellenbezeichnung = Stellenbezeichnung;
+            this.Email = Email;
+            this.Verfuegbar = Verfuegbar;
+        }
+    }
+}
