1f4bf3196c7791599d3ca7d45e49e1a064c78616
Michael Gerb <michael.gerb@yahoo.de>
2021-02-18 13:31:34 +0100

    DataModel hinzugefügt

---
+++ 2_Projekt_Restaurant_Personalplanung/2_Projekt_Restaurant_Personalplanung/Dienstplan.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_Projekt_Restaurant_Personalplanung
+{
+    /// <summary>
+    /// Interaktionslogik für Dienstplan.xaml
+    /// </summary>
+    public partial class Dienstplan : UserControl
+    {
+        MainWindow mainWindow;
+        public Dienstplan(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.LoginAnzeigen();
+        }
+    }
+}
